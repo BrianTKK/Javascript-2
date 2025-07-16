c38d2db1812c0923c9076c28e57eb14959a57578
Brian <brianhenriquedp@gmail.com>
2025-07-16 15:20:25 -0400

    Arrow Function
    Exemplo de criação e uso de uma Arrow Function

--- js/script.js
+++ js/script.js
@@ -1,21 +1,6 @@
-const simularValorPassagem = function (qtdAdultos, qtdCriancas, idaVolta) {
-    const PRECO_PASSAGEM = 1000;
-
-    // calculo adulto 
-    const valorAdulto = PRECO_PASSAGEM * qtdAdultos;
-
-    // calculo criança
-    const valorCrianca = (PRECO_PASSAGEM * qtdCriancas) * 0.5;
-
-    if (idaVolta) {
-        return (valorAdulto + valorCrianca) * 2;
-    } else {
-        return valorAdulto + valorCrianca;
-    }
+const calcularValorHospedagem = (diarias, valorDiaria, taxaLimpeza) => {
+    return diarias * valorDiaria + taxaLimpeza;
 }
 
-let valorPassagem = simularValorPassagem(2, 3, false);
-console.log(`O valor da passagem é: R$ ${valorPassagem.toFixed(2)}`);
-
-valorPassagem = simularValorPassagem(3, 0, true);
-console.log(`O valor da passagem é: R$ ${valorPassagem.toFixed(2)}`);
+const valorHospedagem = calcularValorHospedagem(5, 100, 50);
+console.log(`O valor total da hospedagem é: R$ ${valorHospedagem}`);
