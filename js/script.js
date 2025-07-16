912328c8e486be9ea89bfbb97127ca8ea96f18f0
Brian <brianhenriquedp@gmail.com>
2025-07-16 16:13:22 -0400

    Calllback Function
    Exemplo de uso de callback function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-const calcularValorHospedagem = (diarias, valorDiaria, taxaLimpeza) => {
-    return diarias * valorDiaria + taxaLimpeza;
+function calcularDesconto(preco, porcentagemDesconto) {
+	const desconto = (preco * porcentagemDesconto) / 100;
+	return preco - desconto;
 }
 
-const valorHospedagem = calcularValorHospedagem(5, 100, 50);
-console.log(`O valor total da hospedagem é: R$ ${valorHospedagem}`);
+function calcularPrecoFinal(preco, porcentagemDesconto, callback) {
+	const precoFinal = callback(preco, porcentagemDesconto);
+	console.log(`O preço final após o desconto é: R$ ${precoFinal.toFixed(2)}`);
+}
+
+console.log('Calculando o preço final com desconto...');
+calcularPrecoFinal(100, 10, calcularDesconto);
