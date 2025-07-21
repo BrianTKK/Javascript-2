90e03e657d92b6969b249bf36fae571ee7056615
Brian <brianhenriquedp@gmail.com>
2025-07-21 17:52:36 -0400

    Percorrendo array

--- js/script.js
+++ js/script.js
@@ -1,40 +1,37 @@
-const produtos = ['Notebook', 'Tablet', 'Smartphone', 'Desktop', 'Smartwatch'];
-const precos = [2500, 1500, 1200, 3000, 800];
+const produtos = [
+	'Celular',
+	'Notebook',
+	'Tablet',
+	'Smartwatch',
+	'Fone de Ouvido',
+	'Câmera',
+	'Impressora',
+	'Monitor',
+	'Teclado',
+	'Mouse'
+];
 
-console.log(produtos);
-console.log(precos);
+console.table(produtos);
 
-// filter
-const produtosComSmart = produtos.filter((produto) => {
-	return produto.includes('Smart');
+// percorrendo com for
+for (let i = 0; i < produtos.length; i++) {
+	console.log(produtos[i]);
+}
+console.log('--------------------------------------');
+
+// usando for in
+for (let index in produtos) {
+	console.log(produtos[index]);
+}
+console.log('--------------------------------------');
+
+// usando for of (Não trabalha com indice)
+for (let produto of produtos) {
+	console.log(produto);
+}
+console.log('--------------------------------------');
+
+// usando forEach
+produtos.forEach((produto, index) => {
+	console.log(`${index} - ${produto}`);
 });
-console.log(produtosComSmart);
-
-// filter buscando por preço maior que 2000
-const precosMaiores = precos.filter((preco) => {
-	return preco > 2000;
-});
-console.log(precosMaiores);
-
-// map - transformando os produtos em maiúsculas retornando em um novo array
-const produtosEmMaiuscula = produtos.map((produto) => {
-	return produto.toUpperCase();
-});
-console.log(produtosEmMaiuscula);
-
-// map - dar desconto de 10%
-const precosDesconto = precos.map((preco) => {
-	return preco * 0.9;
-});
-console.log(precosDesconto);
-
-// reduce - retorna um unico valor
-const totalLetrasProdutos = produtos.reduce((acumulador, produto) => {
-	return acumulador + produto.length;}, 0);
-console.log(totalLetrasProdutos);
-
-// reduce - soma todos os preços
-const totalValores = precos.reduce((acumulador, preco) => {
-	return acumulador + preco;
-}, 0);
-console.log(totalValores);
